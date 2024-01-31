fe966c4298df6b2c126572d6cc739befeb188628
kutroff <nikolajkutrov@gmail.com>
2024-01-31 11:08:50 +0300

    Доработки кода и функции обработчика

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import pandas
 import schedule
 
 
-bot = telebot.TeleBot('6724361419:AAGOgTf4eamG4bgwKGAfMAbaepxG0kVIQbc')
+bot = telebot.TeleBot('6724361419:AAG__KMiGEwnDSSwUFXFiyPxjo7MeYqgEHY')
 
 
 def download_file():
@@ -24,7 +24,7 @@ def download_file():
 
 
 schedule.every().day.at("04:50").do(download_file)
-schedule.every().day.at("13:10").do(download_file)
+schedule.every().day.at("8:00").do(download_file)
 
 
 @bot.message_handler(commands=['start'])
@@ -58,7 +58,14 @@ def callback_groups(call, class_number):
     rasp_text1 = pandas.read_excel('C:\\Users\\Nikolai\\Desktop\\telebot\\downloaded_file.xlsx', usecols=colms)
     rasp_text1 = rasp_text1.fillna('')
     rasp_text1_str = rasp_text1.to_string(index=False).replace(" ", "")
+    lines = rasp_text1_str.split('\n')
+    filter_lines = [line for line in lines if not line.strip().endswith(("1.", "2.", "3.", "4.", "5.", "6.", "7."))]
+    for i in range(len(filter_lines)):
+        if filter_lines[i].strip().startswith(("1.", "2.", "3.", "4.", "5.", "6.", "7.")):
+            filter_lines[i] = "      " + filter_lines[i]
+    rasp_text1_str = "\n".join(filter_lines)
     bot.send_message(call.message.chat.id, rasp_text1_str)
+
     btn_return = types.InlineKeyboardButton(
         text='Вернуться назад',
         callback_data='return back'
@@ -79,7 +86,6 @@ def send_contact(message):
     bot.send_message(message.chat.id, 'контакты разработчика - @nikktov')
 
 
-    
 @bot.message_handler(commands=['support'])
 def send_support(message):
     bot.send_message(message.chat.id, 'проблемы с ботом или он не работает - пишите тг @nikktov')
@@ -91,11 +97,11 @@ if __name__ == '__main__':
     while True:
         try:
             bot.polling(none_stop=True)
+            schedule.run_pending()
+            time.sleep(1)
         except Exception as e:
-            time.sleep(3)
             print(e)
+            time.sleep(3)
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
-#'/home/NikKt/telebot/downloaded_file.xlsx'
+
+
