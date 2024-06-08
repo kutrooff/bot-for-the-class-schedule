686ea7649f84f0977aeb9c2ddb99b91288ccf0cf
kutroff <nikolajkutrov@gmail.com>
2024-06-08 11:49:14 +0300

    окончательные правки, сделанные в проекте

--- 1.py
+++ 1.py
@@ -1,10 +1,84 @@
+import pandas
 import re
 
-# Исходная строка
-s = "bhjsjdhb          jsgdfhswegf                    wefygwuf                                iuhefihwife     shfs"
 
-# Удаление лишних пробелов и замена их на один пробел
-cleaned_string = re.sub(r'\s+', ' ', s)
+def get_a_week_teachers():
+    rasp_text1 = pandas.read_excel('downloaded_file.xlsx', index_col=0, usecols="B:L", skiprows=1)
+    rasp_text1_str = rasp_text1.to_string()
+    rasp_text1_lst = rasp_text1_str.split("\n")
+    cl_list = []
+    for item in rasp_text1_lst:
+        item_s = re.sub(r'\s+', ' ', item)
+        cl_list.append(item_s)
 
-# Вывод результата
-print(cleaned_string)
+    del cl_list[0]
+    new_cl_list = [] # массив из уроков для каждого класса их 47
+    for list1 in cl_list:
+        new_list = list1.split(" ")
+        new_cl_list.append(new_list)
+    len_list2 = len(teachers)
+
+    list_new = [[] for _ in range(len_list2)]
+
+    teacher_index = {}
+    for i in range(len_list2):
+        teacher_index[str(i + 1)] = i
+    k = 1
+    for lesson_classes in new_cl_list:
+        # номер урока
+
+        # print(lesson_classes)
+
+        for lesson_class in lesson_classes:
+            start_index = lesson_class.rfind('(')  # Находим индекс начала круглых скобок
+            end_index = lesson_class.rfind(')')  # Находим индекс конца круглых скобок
+            num_teacher = 0
+            if start_index != -1 and end_index != -1:
+                str1 = f"{k}. {lesson_class} - Класс {lesson_classes.index(lesson_class)+1} \n"
+                num_teacher = int(lesson_class[start_index + 1:end_index])
+                list_new[num_teacher-1].append(str1)
+        k = k + 1
+    #         teacher_number = 0
+    #         if all(x == lesson_classes[0] for x in lesson_classes):
+    #             return
+    #         if lesson_class[-3] == "(":
+    #             teacher_number = lesson_class[-2]
+    #         else:
+    #             teacher_number = lesson_class[-3:-1]
+    #         if teacher_number in teacher_index:
+    #             index = teacher_index[teacher_number]
+    #             # номер класса
+    #             i = lesson_classes.index(lesson_class) + 1
+    #             str1 = f"{k}. " + lesson_class + f" - Класс {i}\n"
+    #             # if lesson_class == lesson_classes[-1]:
+    #             #     print("shdbfjshbfjus")
+    #             #     k = k + 1
+    #             k = k + 1
+    #             print(str1)
+    #             list_new[index].append(f"{k}. " + lesson_class + f" - Класс {i}\n")
+    #
+    #     # print(list_new)
+    # list_lesson = "\n".join(["".join(sublist) for sublist in list_new])
+    for item in list_new:
+        print(item)
+
+
+def get_teachers_from_excel():
+    df = pandas.read_excel("downloaded_file.xlsx", index_col=0, usecols="O:O", skiprows=1)
+    df = str(df)
+    df = df.replace(", nan", "")
+    string = df
+    start_index = string.rfind('[')  # Находим индекс начала квадратных скобок
+    end_index = string.rfind(']')  # Находим индекс конца квадратных скобок
+    if start_index != -1 and end_index != -1:
+        substring = string[start_index + 1:end_index]  # Используем срез для получения строки между квадратными скобками
+        df = substring.split(", ")
+    return df
+
+
+teachers = get_teachers_from_excel()
+
+
+
+# print(get_a_week_students())
+get_a_week_teachers()
--- main.py
+++ main.py
@@ -7,8 +7,10 @@ import pandas
 import schedule
 import numpy as np
 import re
+import datetime
+import os
 
-bot = telebot.TeleBot('6350527159:AAFA5ceDIZ4DJNuBhoTFDh67lSMJFdYjxcQ')
+bot = telebot.TeleBot('6027502149:AAFA_0K0zqTdwAcHCDiV9kLh7fB8c7h9qiw')
 
 
 def download_file():
@@ -20,7 +22,8 @@ def download_file():
     download_url = response.json()['href']
     # Загружаем файл и сохраняем его
     download_response = requests.get(download_url)
-    with open('C:\\Users\\Nikolai\\Desktop\\telebot\\downloaded_file.xlsx', 'wb') as f:
+    file_path = 'downloaded_file.xlsx'
+    with open(file_path, 'wb') as f:
         f.write(download_response.content)
 
 
@@ -33,32 +36,20 @@ schedule.every().day.at("13:00").do(job)
 
 
 class User:
-    def __init__(self, user_id, role, text_user):
-        self.user_id = user_id
+    def __init__(self, role, text_user):
         self.role = role
         self.text_user = text_user
 
-    def multiply(self):
-        return f"{self.user_id} и {self.role} и {self.text_user}"
+    def get_role(self):
+        return self.role
 
+    def get_text_user(self):
+        return self.text_user
 
-# Словарь для хранения объектов пользователей
-users = {}
-
-
-# Функция для получения или создания объекта пользователя
-def get_or_create_user(user_id, role, text_user):
-    if user_id not in users:
-        users[user_id] = User(user_id, role, text_user)
-    else:
-        users[user_id].role = role
-        users[user_id].text_user = text_user
-    print(users[user_id].multiply())
 
 
 def get_teachers_from_excel():
-    file_loc = 'C:\\Users\\Nikolai\\Desktop\\telebot\\downloaded_file.xlsx'
-    df = pandas.read_excel(file_loc, index_col=0, usecols="O:O", skiprows=1)
+    df = pandas.read_excel("downloaded_file.xlsx", index_col=0, usecols="O:O", skiprows=1)
     df = str(df)
     df = df.replace(", nan", "")
     string = df
@@ -68,6 +59,108 @@ def get_teachers_from_excel():
         substring = string[start_index + 1:end_index]  # Используем срез для получения строки между квадратными скобками
         df = substring.split(", ")
     return df
+# Словарь для хранения объектов пользователей
+def get_a_week_students():
+    # keyboard = types.InlineKeyboardMarkup()
+    rasp_text1 = pandas.read_excel('downloaded_file.xlsx', index_col=0, usecols="B:L", skiprows=1)
+    rasp_text1_str = rasp_text1.to_string()
+    rasp_text1_lst = rasp_text1_str.split("\n")
+
+    cl_list = []
+    for item in rasp_text1_lst:
+        item_s = re.sub(r'\s+', ' ', item)
+        cl_list.append(item_s)
+
+    del cl_list[0]
+
+    classes_list = [[], [], [], [], [], [], [], [], [], [], []]
+
+    for i in range(len(cl_list)):
+        list1 = cl_list[i].split(" ")
+        for i in range(len(list1)):
+            classes_list[i].append(list1[i])
+
+    pattern = r'(Naц\s*)+'
+
+    # Применяем замену к каждому элементу внутренних списков
+    for i in range(len(classes_list)):
+        classes_list_str = ' '.join(classes_list[i])
+        classes_list_str = re.sub(pattern, 'NaN ', classes_list_str)
+        # После замены преобразуем строку обратно в список
+        classes_list[i] = classes_list_str.split()
+
+    sort_item = []
+    for item in classes_list:
+        sort_item.append("\n".join(item))
+
+    all_rasp = []
+    for item in sort_item:
+        if item == sort_item[0]:
+            item = "\n" + item
+        all_rasp.append(item.split("NaN"))
+
+    for sublist in all_rasp:
+        for i, line in enumerate(sublist):
+            # Разделение строки по символу '\n' и удаление пустых строк
+            subjects = [subj.strip() for subj in line.split('\n') if subj.strip()]
+
+            # Пронумерование и сбор строк
+            new_line = ""
+            for j, subj in enumerate(subjects, 1):
+                new_line += f"{j}. {subj}\n"
+
+            # Замена строки в исходном списке измененной строкой
+            sublist[i] = new_line
+
+    teachers = get_teachers_from_excel()
+    schedule = all_rasp
+    new_schedule = []  # Создаем новый список для модифицированных строк
+    for group_schedule in schedule:
+        new_group_schedule = []  # Создаем новый список для модифицированных строк в текущем списке расписания
+        for item in group_schedule:
+            subjs = item.split("\n")
+            new_subjs = []  # Создаем новый список для модифицированных предметов в текущей строке
+            for subj in subjs:
+                start_index = subj.rfind('(')  # Находим индекс начала круглых скобок
+                end_index = subj.rfind(')')  # Находим индекс конца круглых скобок
+                if start_index != -1 and end_index != -1:
+                    num_teacher = int(subj[start_index + 1:end_index])
+                    new_subj = subj[:start_index] +" — "+ teachers[num_teacher - 1]  # Заменяем подстроку
+                    new_subjs.append(new_subj)
+                else:
+                    new_subjs.append(subj)  # Если круглых скобок нет, просто добавляем предмет без изменений
+            new_group_schedule.append(
+                "\n".join(new_subjs))  # Объединяем модифицированные предметы в одну строку и добавляем в новый список
+        new_schedule.append(new_group_schedule)  # Добавляем новый список расписания для текущей группы в общий список
+    # print(new_schedule)
+    return new_schedule
+
+
+# def get_a_week_teachers():
+#     rasp_text1 = pandas.read_excel('downloaded_file.xlsx', index_col=0, usecols="B:L", skiprows=1)
+#     rasp_text1_str = rasp_text1.to_string()
+#     rasp_text1_lst = rasp_text1_str.split("\n")
+#     print(rasp_text1_lst)
+#
+#
+#
+# a = get_a_week_teachers()
+
+# print(a)
+users = {}
+all_rasp_for_students = get_a_week_students()
+
+
+# Функция для получения или создания объекта пользователя
+def get_or_create_user(user_id, role, text_user):
+    if user_id not in users:
+        users[user_id] = User(role, text_user)
+    else:
+        users[user_id].role = role
+        users[user_id].text_user = text_user
+
+
+
 
 
 def send_teacher_options(call):
@@ -99,6 +192,16 @@ def send_student_options(call):
     bot.send_message(call.message.chat.id, "Привет, выберите класс: ", reply_markup=keyboard)
 
 
+def get_buttons(call):
+    keyboard = [
+        [types.InlineKeyboardButton("Сегодня", callback_data='on_today')],
+        [types.InlineKeyboardButton("Завтра", callback_data='on_tomorrow')],
+        [types.InlineKeyboardButton("На неделю", callback_data='on_week')]
+    ]
+    reply_markup = types.InlineKeyboardMarkup(keyboard)
+    bot.send_message(call.message.chat.id, "Получить расписание на:", reply_markup=reply_markup)
+
+
 @bot.message_handler(commands=['start'])
 def start(message):
     keyboard = [
@@ -111,28 +214,119 @@ def start(message):
     # update.message.reply_text('Выберите роль:', reply_markup=reply_markup)
 
 
+def delete_all_messages(call):
+    bot.delete_message(call.message.chat.id, call.message.message_id - 1)
+
+    # try:
+    #     # Получение всех сообщений в чате
+    #     messages = bot.sent_messages
+    #
+    #     # Удаление каждого сообщения, отправленного ботом
+    #     for message in messages:
+    #         if message.chat.id == chat_id:
+    #             bot.delete_message(chat_id, message.message_id)
+    #
+    #     # Оповещение об удалении сообщений
+    #     bot.send_message(chat_id, "Все предыдущие сообщения были удалены.")
+    # except Exception as e:
+    #     print("Ошибка при удалении сообщений:", e)
+
+
+def get_rasp_on_week(class_index):
+    rasps = all_rasp_for_students
+    class_index -= 1
+    count_day = 0
+
+    # Список дней недели
+    days_of_week = ['Понедельник:', 'Вторник:', 'Cреда:', 'Четверг:', 'Пятница:', 'Суббота:', "Воскресенье"]
+
+    # Проверяем, что группа с таким индексом существует в списке rasps
+    if class_index < len(rasps):
+        # Проверяем, что день недели существует в расписании для данной группы
+        # if count_day < len(rasps[class_index]):
+        str_rasp = ""
+        for item in rasps[class_index]:
+            if count_day == 6:
+                str_rasp += days_of_week[count_day] + "\n" + "Выходной"
+                return str_rasp
+            str_rasp += days_of_week[count_day] + "\n" + item + "\n"
+            count_day += 1
+    # else:
+    #     return "День недели не найден в расписании для этой группы"
+    else:
+        return "Группа не найдена в расписании"
+
+
+def get_rasp_today_or_tomorrow(class_index, tomorrow):
+    rasps = all_rasp_for_students
+    class_index -= 1
+    # Получаем текущий день недели
+    today = datetime.datetime.today().weekday()
+
+    if tomorrow:
+        today += 1
+
+    if today == 7:
+        today = 0
+    # Список дней недели
+    days_of_week = ['Понедельник:', 'Вторник:', 'Cреда:', 'Четверг:', 'Пятница:', 'Суббота:', "Воскресенье"]
+    # print(f"class_index:{class_index}")
+    # Проверяем, что группа с таким индексом существует в списке rasps
+    if class_index < len(rasps):
+        # Проверяем, что день недели существует в расписании для данной группы
+        if today < len(rasps[class_index]):
+            if today == 6:
+                return days_of_week[today] + "\n" + "Выходной"
+            return days_of_week[today] + "\n" + rasps[class_index][today]
+        else:
+            return "День недели не найден в расписании для этой группы"
+    else:
+        return "Группа не найдена в расписании"
+
+
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
     if call.data == 'teacher':
-        # Отправляем выборку для учителя
         send_teacher_options(call)
     elif call.data == 'student':
-        # Отправляем выборку для ученика
         send_student_options(call)
     elif 'Класс ' in call.data:
+        # print(users)
         get_or_create_user(call.from_user.id, "Ученик", call.data)
-        class_num = int(call.data[6:])
-        get_a_week(call, class_num)
-    elif call.data:
-        print(call.data)
+        # print(users)
+        # delete_all_messages(call)
+        bot.send_message(call.message.chat.id, "Ваши настройки сохранены")
+        get_buttons(call)
+    elif call.data == "on_today":
+        id = call.from_user.id
+        # print(call.from_user.id)
+        # print(users[id].get_role())
+        # print(users[id].get_text_user())
+        if users[id].get_role() == "Ученик":
+            match = re.search(r'Класс (\d+)', users[id].get_text_user())
+            class_index = int(match.group(1))
+            # print(f"cl:{class_index}")
+            rasp = get_rasp_today_or_tomorrow(class_index, False)
 
+            bot.send_message(call.message.chat.id, rasp)
+    elif call.data == "on_tomorrow":
+        id = call.from_user.id
+        if users[id].get_role() == "Ученик":
+            match = re.search(r'Класс (\d+)', users[id].get_text_user())
+            class_index = int(match.group(1))
+            # print(f"cl:{class_index}")
+            rasp = get_rasp_today_or_tomorrow(class_index, True)
 
-# @bot.callback_query_handler(func=lambda call: True)
-# def callback_query(call):
-#     print(call.data)
-#     user_id = call.from_user.id
-#     class_selected = call.data
-#     get_or_create_user(user_id, "Ученик", class_selected)
+            bot.send_message(call.message.chat.id, rasp)
+    elif call.data == "on_week":
+        id = call.from_user.id
+        if users[id].get_role() == "Ученик":
+            match = re.search(r'Класс (\d+)', users[id].get_text_user())
+            class_index = int(match.group(1))
+            # print(f"cl:{class_index}")
+            rasp = get_rasp_on_week(class_index)
+            # print(rasp)
+            bot.send_message(call.message.chat.id, rasp)
 
 
 @bot.callback_query_handler(func=lambda call: True)
@@ -141,88 +335,7 @@ def callback_querry(call):
         start(call.message)
     else:
         callback_data = int(call.data)
-        get_a_week(call, callback_data)
-
-
-def get_a_week(call, class_number):
-    keyboard = types.InlineKeyboardMarkup()
-    rasp_text1 = pandas.read_excel('C:\\Users\\Nikolai\\Desktop\\telebot\\downloaded_file.xlsx', index_col=0, usecols="B:L", skiprows=1)
-    rasp_text1_str = rasp_text1.to_string()
-    rasp_text1_lst = rasp_text1_str.split("\n")
-
-    cl_list = []
-    for item in rasp_text1_lst:
-        item_s = re.sub(r'\s+', ' ', item)
-        cl_list.append(item_s)
-
-    del cl_list[0]
-
-
-    cl_list = [line for line in cl_list if not all(word == 'NaN' for word in line.split())]
-
-    classes_list = [[],[],[],[],[],[],[],[],[],[],[]]
-    print(len(cl_list))
-    for i in range(len(cl_list)):
-        list1 = cl_list[i].split(" ")
-        for i in range(len(list1)):
-            classes_list[i].append(list1[i])
-
-    print(classes_list)
-
-    # for i in range(11):
-    #     list_classes[i] = []
-    #     for j in range(6):
-    #         list_classes[i][j] = []
-
-    # for item in rasp_text1_lst:
-    #     item = re.sub(r'\s+', ' ', item)
-    # print(rasp_text1_lst)
-    #     if item == "":
-    #         rasp_text1_lst.remove(item)
-    # cleaned_lines = [line for line in rasp_text1_lst if "NaN" not in line]
-    # cleaned_lines.pop(0)
-
-    # for item in rasp_text1_lst:
-    #     list1 = item.split(" ")
-    #     print(list1)
-    # Вывод результата
-    # print(cleaned_lines)
-
-    # rasp_text1_lst.pop(0)
-    # for i in range(11):
-    #     rasp_text1_lst.pop(i)
-
-    # colms = [0, class_number]
-    # rasp_text1 = pandas.read_excel('C:\\Users\\Nikolai\\Desktop\\telebot\\downloaded_file.xlsx', usecols=colms)
-    # rasp_text1 = rasp_text1.fillna('')
-    # rasp_text1_str = rasp_text1.to_string(index=False).replace(" ", "")
-    # lines = rasp_text1_str.split('\n')
-    # filter_lines = [line for line in lines if not line.strip().endswith(("1.", "2.", "3.", "4.", "5.", "6.", "7."))]
-    # copy_filter_lines = filter_lines
-    # filter_lines.pop(0)
-    # # for i in range(len(filter_lines)):
-    # #     try:
-    # #         print(int(filter_lines[i][0])
-    # #     except ValueError:
-    # #         filter_lines.pop(i)
-    #
-    # for i in range(len(filter_lines)):
-    #     if filter_lines[i].strip().startswith(("1.", "2.", "3.", "4.", "5.", "6.", "7.")):
-    #         filter_lines[i] = "      " + filter_lines[i]
-    # rasp_text1_str = "\n".join(filter_lines)
-    # filter_lines.pop(0)
-    # print(filter_lines)
-    # schedule_week = rasp_text1_str
-    # # print(schedule_week)
-    # bot.send_message(call.message.chat.id, schedule_week)
-
-    btn_return = types.InlineKeyboardButton(
-        text='Вернуться назад',
-        callback_data='return back'
-    )
-    keyboard.add(btn_return)
-    bot.send_message(call.message.chat.id, "Нажмите кнопку...", reply_markup=keyboard)
-
+        # get_a_week(call, callback_data)
 
 @bot.message_handler(commands=['help'])
 def send_help(message):
@@ -249,5 +362,5 @@ if __name__ == '__main__':
             schedule.run_pending()
             time.sleep(1)
         except Exception as e:
-            print(e)
+            # print(e)
             time.sleep(3)
